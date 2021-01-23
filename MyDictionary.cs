86adfe26ad00ee7bd4fac375536daf1fc1cd6be5
MRALADAG <mstf_aladag@yahoo.com>
2021-01-23 23:48:39 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<T>
+    {
+        T[] elements;
+        public MyDictionary()
+        {
+            elements = new T[0];
+        }
+        public void Add(T element)
+        {
+            T[] tempArray = elements;
+            elements = new T[elements.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                elements[i] = tempArray[i];
+            }
+            elements[elements.Length - 1] = element;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+            myDictionary.Add("Ahmet");
+            myDictionary.Add("Engin");
+            myDictionary.Add("Seçkin");
+            Console.WriteLine(myDictionary);
+        }
+    }
+}
